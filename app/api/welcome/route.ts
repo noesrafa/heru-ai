857923cba2c3e330c0056a7e626b0a93a2f11f26
Rafael Alexander <rafael.tena@heru-app.com>
2023-12-12 13:10:39 -0600

    fix

--- app/api/welcome/route.ts
+++ app/api/welcome/route.ts
@@ -1,46 +1,11 @@
 import { welcomeFunctions } from "@/utils/functions";
 import { welcomePrompt } from "@/utils/prompts";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY_CUSTOM,
 });
-
-const getCorsHeaders = (origin: string) => {
-  const headers = {
-    "Access-Control-Allow-Methods": `${process.env.ALLOWED_METHODS}`,
-    "Access-Control-Allow-Headers": `${process.env.ALLOWED_HEADERS}`,
-    "Access-Control-Allow-Origin": `${process.env.DOMAIN_URL}`,
-  };
-
-  // If no allowed origin is set to default server origin
-  if (!process.env.ALLOWED_ORIGIN || !origin) return headers;
-
-  // If allowed origin is set, check if origin is in allowed origins
-  const allowedOrigins = process.env.ALLOWED_ORIGIN.split(",");
-
-  // Validate server origin
-  if (allowedOrigins.includes("*")) {
-    headers["Access-Control-Allow-Origin"] = "*";
-  } else if (allowedOrigins.includes(origin)) {
-    headers["Access-Control-Allow-Origin"] = origin;
-  }
-
-  // Return result
-  return headers;
-};
-
-export const OPTIONS = async (request: NextRequest) => {
-  // Return Response
-  return NextResponse.json(
-    {},
-    {
-      status: 200,
-      headers: getCorsHeaders(request.headers.get("origin") || ""),
-    }
-  );
-};
 
 export async function POST(request: Request) {
   const { message } = await request.json();
@@ -97,10 +62,7 @@ export async function POST(request: Request) {
       "\n\n -------------------------------------- \n\n "
     );
 
-    return NextResponse.json(response, {
-      status: 200,
-      headers: getCorsHeaders(request.headers.get("origin") || ""),
-    });
+    return NextResponse.json(response);
   } catch (error) {
     return NextResponse.json(
       {
