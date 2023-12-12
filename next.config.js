60b9e2274740eb8aa968bcb4db392af6ed696cdc
Rafael Alexander <rafael.tena@heru-app.com>
2023-12-12 12:49:29 -0600

    fix recommendation

--- next.config.js
+++ next.config.js
@@ -3,27 +3,26 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
-        source: "/api/:path*",
+        // Routes this applies to
+        source: "/api/(.*)",
+        // Headers
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
+          // Allow for specific domains to have access or * for all
           {
             key: "Access-Control-Allow-Origin",
-            value: "http://localhost:5173/",
-          }, // replace this your actual origin
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "http://localhost:5173",
-          }, // replace this your actual origin
-          { key: "Access-Control-Allow-Origin", value: "localhost:5173" }, // replace this your actual origin
+            value: "*",
+            // DOES NOT WORK
+            // value: process.env.ALLOWED_ORIGIN,
+          },
+          // Allows for specific methods accepted
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
           },
+          // Allows for specific headers accepted (These are a few standard ones)
           {
             key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+            value: "Content-Type, Authorization",
           },
         ],
       },
