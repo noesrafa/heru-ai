5dc80e715dba6b6e14efcbfa80bf13d5e026ea59
Rafael Alexander <rafael.tena@heru-app.com>
2024-01-24 10:29:58 -0600

    Update prompt

--- app/api/hubspot-webhook-v2/route.ts
+++ app/api/hubspot-webhook-v2/route.ts
@@ -144,7 +144,7 @@ export async function POST(request: Request) {
 
     return NextResponse.json({
       botMessage: responseJson.choices?.[0].message.content?.replaceAll(
-        "/",
+        "\\",
         ""
       ),
       nextModuleNickname: "",
